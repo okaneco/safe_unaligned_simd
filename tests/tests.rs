ddce405981f2b84b8da651cfce53e4ec7aa7beb7
okaneco <47607823+okaneco@users.noreply.github.com>
2025-07-25 22:46:43 -0400

    Lay foundation for assembly codegen tests
    Add compiletest_rs for assembly code gen tests
    Install LLVM tools in CI for filecheck
    
    Add sse assembly test

---
+++ tests/tests.rs
@@ -0,0 +1,29 @@
+#[allow(dead_code)]
+fn run_mode(mode: &'static str, custom_dir: Option<&'static str>) {
+    let mut config = compiletest_rs::Config::default();
+    let cfg_mode = mode.parse().expect("Invalid mode");
+
+    config.mode = cfg_mode;
+
+    let dir = custom_dir.unwrap_or(mode);
+    config.src_base = std::path::PathBuf::from(format!("tests/{}", dir));
+    config.target_rustcflags = Some("-L target/debug -L target/debug/deps".to_string());
+    config.clean_rmeta(); // If your tests import the parent crate, this helps with E0464
+    config.clean_rlib();
+    config.strict_headers = true;
+    config.llvm_filecheck = Some(
+        std::env::var("FILECHECK")
+            .unwrap_or("FileCheck".to_string())
+            .into(),
+    );
+
+    compiletest_rs::run_tests(&config);
+}
+
+#[test]
+fn compile_test() {
+    // run_mode("compile-fail", None);
+    // run_mode("run-pass", None);
+    #[cfg(feature = "assembly")]
+    run_mode("assembly", None);
+}
